4dbec7ec848fa978dbe75970f3d5eedf1bf5c045
АлександÑAleksandr Ryazantsev <amapkymap@gmail.com>
2022-08-28 23:05:55 +0300

    faker fixed commit

--- src/test/java/ChangeDateFakerTest.java
+++ src/test/java/ChangeDateFakerTest.java
@@ -1,8 +1,5 @@
 import com.codeborne.selenide.Configuration;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static com.codeborne.selenide.Condition.*;
 
