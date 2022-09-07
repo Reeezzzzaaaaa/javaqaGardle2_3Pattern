617901e3b45ec38fe27882700a1b3185eeb88bc9
АлександÑAleksandr Ryazantsev <amapkymap@gmail.com>
2022-09-07 22:20:58 +0300

    fixed 2 commit

--- src/test/java/сardDelivery/test/ChangeDateFakerTest.java
+++ src/test/java/сardDelivery/test/ChangeDateFakerTest.java
@@ -7,16 +7,22 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.open;
 
 public class ChangeDateFakerTest {
 
-    LoginPage login = new LoginPage();
+    @BeforeEach
+    void openPage() {
+        open("http://localhost:9999/");
+    }
+
     RegistrationInfo info = DataGenerator.Data.generateInfo("ru");
 
     @Test
     void shouldBeValidTest() {
 
         Configuration.holdBrowserOpen = true;
+        LoginPage login = new LoginPage();
         login.planDate(info.getCity(), 3, info.getName(), info.getPhone(), appear, 12);
         login.shouldHave(3, 12, visible);
         login.rePlanDate(10, appear, 12);
